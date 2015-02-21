8900d35c3c63e4b7543a12e0152914db2d5635ac
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-02-21 12:43:19 -0800

    added navbar active state functionality

--- app/assets/javascripts/utilities/navbar.js
+++ app/assets/javascripts/utilities/navbar.js
@@ -3,30 +3,45 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 
-$(document).ready(function () {
+var my_affix;
+var nav_active;
 
-  /* affix the navbar after scroll below header */
+my_affix = function() {
   $('#nav').affix({
     offset: {
-      top: 100
+      top: 70
     }
   });
 
   /* highlight the top nav as scrolling occurs */
-  $('body').scrollspy({ target: '#nav' })
+  //$('body').scrollspy({ target: '#nav' })
 
   /* smooth scrolling for scroll to top */
-  $('.scroll-top').click(function(){
-    $('body,html').animate({scrollTop:0},1000);
-  })
+  //$('.scroll-top').click(function(){
+    //$('body,html').animate({scrollTop:0},1000);
+  //})
 
   /* smooth scrolling for nav sections */
-  $('#nav .navbar-nav li>a').click(function(){
-    var link = $(this).attr('href');
-    var posi = $(link).offset().top;
-    $('body,html').animate({scrollTop:posi},700);
-  });
+  //$('#nav .navbar-nav li>a').click(function(){
+    //var link = $(this).attr('href');
+    //var posi = $(link).offset().top();
+    //$('body,html').animate({scrollTop:posi},700);
+  //});
+};
 
-});
+nav_active = function() {
+  var url = window.location;
 
+  $("#nav .navbar-nav").find(".active").removeClass("active");
+  $('ul.nav a').filter(function() {
+      return this.href == url;
+  }).parent().addClass('active');
+};
+
+
+$(document).ready(my_affix);
+$(document).ready(nav_active);
+
+$(document).on('page:load', my_affix);
+$(document).on('page:load', nav_active);
 
