d3ff3e6289a5ddf5390fbe2c5b084b473252669e
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-02-22 15:16:29 -0800

    added active admin and admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,7 @@ unless Rails.env.eql?("production")
   developer = User.new(email: 'developers@chicanolegacy.com', password: 'password')
   developer.skip_confirmation!
   developer.save!
+
+  developer = AdminUser.new(email: 'developers@chicanolegacy.com', password: 'password')
+  developer.save!
 end
